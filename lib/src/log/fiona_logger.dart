166af05226c66bdc31a33e4d53bb23680e342e19
Bernardo Iribarne <bernardo.iribarne@empactit.com>
2023-07-14 08:15:25 -0300

    filename fixed

--- lib/src/log/fiona_logger.dart
+++ lib/src/log/fiona_logger.dart
@@ -1,75 +1,65 @@
-
 import 'package:fiona_logger/src/domain/log_data.dart';
-import 'package:fiona_logger/src/domain/log_repository.dart';
+import 'package:fiona_logger/src/domain/log_data_repository.dart';
 import 'package:get_it/get_it.dart';
 
-enum Level {
-  verbose,
-  debug,
-  info,
-  warning,
-  error
-}
+enum Level { verbose, debug, info, warning, error }
 
-class FionaLogger{
-
+class FionaLogger {
   Level level;
   late LogDataRepository logDataRepository;
 
-  FionaLogger({this.level=Level.verbose}){
+  FionaLogger({this.level = Level.verbose}) {
     logDataRepository = GetIt.instance<LogDataRepository>();
   }
 
-  Future<List<LogData>> getAll() async{
+  Future<List<LogData>> getAll() async {
     return logDataRepository.getAll();
   }
 
-  Future<void> clear() async{
+  Future<void> clear() async {
     logDataRepository.clear();
   }
 
   void log(LogData log, Level level) {
-
     int levelIndex = Level.values.indexOf(level);
     int logLevelIndex = Level.values.indexOf(this.level);
 
-    if(logLevelIndex<=levelIndex){
+    if (logLevelIndex <= levelIndex) {
       _writeLog(log);
     }
   }
 
-  void _writeLog(LogData log){
+  void _writeLog(LogData log) {
     logDataRepository.add(log);
   }
 
   void v(String message) {
-    LogData data = LogData(message,level: Level.verbose.toString());
+    LogData data = LogData(message, level: Level.verbose.toString());
     data.datetime = DateTime.now().toUtc().millisecondsSinceEpoch;
-    log( data, Level.verbose);
+    log(data, Level.verbose);
   }
 
   void w(String message) {
-    LogData data = LogData(message,level: Level.warning.toString());
+    LogData data = LogData(message, level: Level.warning.toString());
     data.datetime = DateTime.now().toUtc().millisecondsSinceEpoch;
-    log( data, Level.warning);
+    log(data, Level.warning);
   }
 
   void i(String message) {
-    LogData data = LogData(message,level: Level.info.toString());
+    LogData data = LogData(message, level: Level.info.toString());
     data.datetime = DateTime.now().toUtc().millisecondsSinceEpoch;
-    log( data, Level.info);
+    log(data, Level.info);
   }
 
   void d(String message) {
-    LogData data = LogData(message,level: Level.debug.toString());
+    LogData data = LogData(message, level: Level.debug.toString());
     data.datetime = DateTime.now().toUtc().millisecondsSinceEpoch;
-    log( data, Level.debug);
+    log(data, Level.debug);
   }
 
   void e(String message) {
-    LogData data = LogData(message,level: Level.error.toString());
+    LogData data = LogData(message, level: Level.error.toString());
     data.datetime = DateTime.now().toUtc().millisecondsSinceEpoch;
-    log( data, Level.error);
+    log(data, Level.error);
   }
-
 }
--- lib/src/log/log_in_console.dart
+++ lib/src/log/log_in_console.dart
@@ -1,27 +1,23 @@
 import 'package:fiona_logger/src/domain/log_data.dart';
-import 'package:fiona_logger/src/domain/log_repository.dart';
+import 'package:fiona_logger/src/domain/log_data_repository.dart';
 import 'package:flutter/foundation.dart';
 
-
-class LogInConsole implements LogDataRepository{
-
+class LogInConsole implements LogDataRepository {
   List<LogData> logs = List.empty(growable: true);
-
 
   @override
   Future<void> clear() async {
-      logs = List.empty(growable: true);
+    logs = List.empty(growable: true);
   }
 
   @override
   Future<void> add(LogData logData) async {
-     logs.add(logData);
-     debugPrint(logData.message);
+    logs.add(logData);
+    debugPrint(logData.message);
   }
 
   @override
   Future<List<LogData>> getAll() async {
-    return  logs;
+    return logs;
   }
-
 }
